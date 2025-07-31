479e2c918c1514b4f4d48413fc66bccdfc039317
Vikas Basavi <156122503+Vikasvs91@users.noreply.github.com>
2025-07-31 11:02:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,10 +34,7 @@ app.use('/api', applyRoute);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
 
